0647e664d61edc6e5cf44b33e20091b8d716d184
ellingworthl <ellingworthl@hotmail.com>
2017-04-05 20:01:07 +0100

    About.js & registerApp.js
    static text update

--- src/registerApp.js
+++ src/registerApp.js
@@ -7,10 +7,7 @@ var RegisterApp = React.createClass({
 
         <h1>Register as a User of FAF</h1>
 
-        <p>As a registered user of this app you will be able to submit reviews on shoots/events attended.</p>
-        
-        <p>Registered users can also request their venue be added to the Venues page. Send FAF an <a href="mailto:FAFAdmin@gmail.com?subject=Register a Venue">email</a> with all of the details.</p>
-                
+        <p>As a registered user of this you can receive the FAF Newsletter.</p>            
 
         <p>CONTENT UNDER CONSTRUCTION</p>
 
